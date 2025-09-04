54112565fe4f3f6546fef404d711476ef3a956e0
Siddhi Bajpai <bajpaisiddhi20@gmail.com>
2025-09-04 14:43:40 +0530

    Create exp11(c).cpp

---
+++ exp11(c).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class cube
+{
+    private:
+    int height=2.0;
+    int width=3.0;
+    int length=5.0;
+    public:
+    int volume()
+    {
+        int v;
+        v=height*width*length;
+        return v;
+    }
+};
+int main()
+{
+    cube cube1;
+    int vol=cube1.volume();
+    cout<<"Volume:"<<vol<<endl;
+}
+
+//Output:
+//Volume:30
